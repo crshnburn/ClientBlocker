3e344f0f130bd78e0c4d8f504cb9c60f37dc7945
Andrew Smithson <smithson@uk.ibm.com>
2014-11-13 16:19:57 +0000

    Add code copyright header

--- com/ibm/ctg/samples/clientblocker/ClientBlocker.java
+++ com/ibm/ctg/samples/clientblocker/ClientBlocker.java
@@ -1,3 +1,19 @@
+/*
+ Copyright IBM Corporation 2014
+
+ LICENSE: Apache License
+          Version 2.0, January 2004
+          http://www.apache.org/licenses/
+
+ The following code is sample code created by IBM Corporation.
+ This sample code is not part of any standard IBM product and
+ is provided to you solely for the purpose of assisting you in
+ the development of your applications.  The code is provided
+ 'as is', without warranty or condition of any kind.  IBM shall
+ not be liable for any damages arising out of your use of the
+ sample code, even if IBM has been advised of the possibility
+ of such damages.
+*/
 package com.ibm.ctg.samples.clientblocker;
 
 import java.net.InetAddress;
@@ -21,13 +37,13 @@ public class ClientBlocker implements CICSRequestExit {
       }
       return server;
    }
-   
+
    @Override
    public int getRetryCount() {
       //Return 0 as we don't want to retry requests in this exit
       return 0;
    }
-   
+
    @Override
    public void eventFired(ExitEvent event, Map<ExitEventData, Object> eventData) {
       switch (event) {
